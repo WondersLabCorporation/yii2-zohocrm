25e83a16005c87ee06381449a8a5a6c2e32e3eb1
wl-dr <wl-dr@wonderslab.com>
2016-03-10 18:55:58 +0200

    ZohoParams added to config

--- Zoho.php
+++ Zoho.php
@@ -24,6 +24,8 @@ class Zoho extends \yii\base\Component
     public $authToken;
     public $baseUri = 'https://crm.zoho.com/crm/private';
 
+    public $zohoApiParams = [];
+
     protected $client;
 
     public function __construct($config = [])
@@ -36,23 +38,34 @@ class Zoho extends \yii\base\Component
 
     public function __call($name, $params)
     {
+
+        if (strpos($name, 'create') === 0) {
+            // Cut 'create' from $name and try to create such and entity. e.g. createAccount => new Account
+            return ZohoRecord::createEntity(substr($name, 6), $this->prepareZohoParams($params));
+        }
+        if (strpos($name, 'load') === 0) {
+            // Cut 'load' from $name and try to create such and entity. e.g. loadAccount => Account -> getRecordById
+            return ZohoRecord::getEntity(substr($name, 4), $this->prepareZohoParams($params));
+        }
+        return parent::__call($name, $params);
+    }
+
+    protected function prepareZohoParams($params)
+    {
         // Using first argument only for now. TODO: Make sure this is the appropriate way
         if (isset($params[0]) && is_array($params[0])) {
             $params = $params[0];
         } else {
             $params = [];
         }
+        // Adding API default params if any.
+        if (!isset($params['zohoParams'])) {
+            $params['zohoParams'] = [];
+        }
+        $params['zohoParams'] += $this->zohoApiParams;
         // TODO: Consider checking if zohoClient provided first
         $params['zohoClient'] = $this->getClient();
-        if (strpos($name, 'create') === 0) {
-            // Cut 'create' from $name and try to create such and entity. e.g. createAccount => new Account
-            return ZohoRecord::createEntity(substr($name, 6), $params);
-        }
-        if (strpos($name, 'load') === 0) {
-            // Cut 'load' from $name and try to create such and entity. e.g. loadAccount => Account -> getRecordById
-            return ZohoRecord::getEntity(substr($name, 4), $params);
-        }
-        return parent::__call($name, $params);
+        return $params;
     }
 
     public function getClient()
